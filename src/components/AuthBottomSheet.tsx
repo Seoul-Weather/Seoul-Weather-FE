a68faed7d4fa2047ca4b3b6387e04cfaf73603cc
Sang-minKIM <87116017+Sang-minKIM@users.noreply.github.com>
2023-05-25 14:35:28 +0900

    feat: Add styling to auth sheet

--- src/components/AuthBottomSheet.tsx
+++ src/components/AuthBottomSheet.tsx
@@ -1,7 +1,10 @@
 import { css } from "@emotion/react";
 import { BottomSheet } from "@qve-ui/qds";
 import Image from "next/image";
-import { useState } from "react";
+import spotIcon from "@/assets/spotIcon.svg";
+import notiIcon from "@/assets/notiIcon.svg";
+import { SubmitButton } from "./SubmitButton";
+import { theme } from "@/styles/theme";
 
 interface IProps {
     isSheet: boolean;
@@ -17,30 +20,28 @@ export const AuthBottomSheet = ({ isSheet, setIsSheet, setIsPush }: IProps) => {
     return (
         <BottomSheet isOpen={isSheet} onClose={() => setIsSheet(false)} xButton={false} ratio={60}>
             <div css={sheet}>
-                <h2>
-                    서비스 사용을 위해
-                    <br />
-                    접근 권한을 허용해주세요.
-                </h2>
-                <section>
-                    <span>선택 권한</span>
-                    <section>
-                        <div>
-                            <Image src="" alt="locationIcon" />
-                            <span>위치</span>
-                            <span>현재 위치의 날씨를 알려드릴게요.</span>
-                        </div>
-                        <div>
-                            <Image src="" alt="alertIcon" />
-                            <span>알림</span>
-                            <span>설정한 시간마다 준비물을 알려드릴게요.</span>
-                        </div>
+                <section css={notiSection}>
+                    <h2 css={header}>
+                        서비스 사용을 위해
+                        <br />
+                        접근 권한을 허용해주세요.
+                    </h2>
+                    <span css={notiText}>선택 권한</span>
+                    <section css={textSection}>
+                        <Image src={spotIcon} width={18} height={20} alt="locationIcon" />
+                        <span css={notiTopic}>위치</span>
+                        <span css={notiText}>현재 위치의 날씨를 알려드릴게요.</span>
+                    </section>
+                    <section css={textSection}>
+                        <Image src={notiIcon} width={18} height={20} alt="alertIcon" />
+                        <span css={notiTopic}>알림</span>
+                        <span css={notiText}>설정한 시간마다 준비물을 알려드릴게요.</span>
                     </section>
                 </section>
-                <span>선택 권한의 경우 허용하지 않아도 서비스를 사용할 수 있으나 일부 서비스 이용이 제한될 수 있습니다.</span>
-                <button type="button" onClick={onClick}>
-                    확인
-                </button>
+                <section>
+                    <div css={notiText}>선택 권한의 경우 허용하지 않아도 서비스를 사용할 수 있으나 일부 서비스 이용이 제한될 수 있습니다.</div>
+                    <SubmitButton onClick={onClick} />
+                </section>
             </div>
         </BottomSheet>
     );
@@ -48,6 +49,38 @@ export const AuthBottomSheet = ({ isSheet, setIsSheet, setIsPush }: IProps) => {
 
 const sheet = css`
     width: 100vw;
-
+    height: 100%;
+    border-radius: 10px;
     background-color: #fff;
+    padding: 30px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+`;
+
+const notiSection = css`
+    margin-top: 10px;
+`;
+
+const header = css`
+    font-size: ${theme.fontSize.large};
+    margin-bottom: 20px;
+`;
+
+const textSection = css`
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+`;
+
+const notiTopic = css`
+    font-size: ${theme.fontSize.regular};
+    margin: 5px 8px;
+`;
+
+const notiText = css`
+    color: ${theme.color.grey};
+    font-size: ${theme.fontSize.small};
+    padding: 10px 0px;
+    text-align: left;
 `;
