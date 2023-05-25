2e1a2ea20cfa8deb1f58190a1fc5e84adac46e0b
Sang-minKIM <87116017+Sang-minKIM@users.noreply.github.com>
2023-05-25 14:54:34 +0900

    refactor: Make notification component

--- src/components/AuthBottomSheet.tsx
+++ src/components/AuthBottomSheet.tsx
@@ -1,11 +1,9 @@
 import { css } from "@emotion/react";
 import { BottomSheet } from "@qve-ui/qds";
-import Image from "next/image";
-import spotIcon from "@/assets/spotIcon.svg";
-import notiIcon from "@/assets/notiIcon.svg";
 import { SubmitButton } from "./SubmitButton";
 import { theme } from "@/styles/theme";
 import { getGeoLocation } from "@/hooks/getGeoLocation";
+import { Notification, notiText } from "./Notification";
 
 interface IProps {
     isSheet: boolean;
@@ -29,16 +27,7 @@ export const AuthBottomSheet = ({ isSheet, setIsSheet, setIsPush }: IProps) => {
                         접근 권한을 허용해주세요.
                     </h2>
                     <span css={notiText}>선택 권한</span>
-                    <section css={textSection}>
-                        <Image src={spotIcon} width={18} height={20} alt="locationIcon" />
-                        <span css={notiTopic}>위치</span>
-                        <span css={notiText}>현재 위치의 날씨를 알려드릴게요.</span>
-                    </section>
-                    <section css={textSection}>
-                        <Image src={notiIcon} width={18} height={20} alt="alertIcon" />
-                        <span css={notiTopic}>알림</span>
-                        <span css={notiText}>설정한 시간마다 준비물을 알려드릴게요.</span>
-                    </section>
+                    <Notification />
                 </section>
                 <section>
                     <div css={notiText}>선택 권한의 경우 허용하지 않아도 서비스를 사용할 수 있으나 일부 서비스 이용이 제한될 수 있습니다.</div>
@@ -67,22 +56,4 @@ const notiSection = css`
 const header = css`
     font-size: ${theme.fontSize.large};
     margin-bottom: 20px;
-`;
-
-const textSection = css`
-    display: flex;
-    justify-content: flex-start;
-    align-items: center;
-`;
-
-const notiTopic = css`
-    font-size: ${theme.fontSize.regular};
-    margin: 5px 8px;
-`;
-
-const notiText = css`
-    color: ${theme.color.grey};
-    font-size: ${theme.fontSize.small};
-    padding: 10px 0px;
-    text-align: left;
 `;
---
+++ src/components/Notification.tsx
@@ -0,0 +1,40 @@
+import { css } from "@emotion/react";
+import spotIcon from "@/assets/spotIcon.svg";
+import notiIcon from "@/assets/notiIcon.svg";
+import Image from "next/image";
+import { theme } from "@/styles/theme";
+
+export const Notification = () => {
+    return (
+        <>
+            <section css={textSection}>
+                <Image src={spotIcon} width={18} height={20} alt="locationIcon" />
+                <span css={notiTopic}>위치</span>
+                <span css={notiText}>현재 위치의 날씨를 알려드릴게요.</span>
+            </section>
+            <section css={textSection}>
+                <Image src={notiIcon} width={18} height={20} alt="alertIcon" />
+                <span css={notiTopic}>알림</span>
+                <span css={notiText}>설정한 시간마다 준비물을 알려드릴게요.</span>
+            </section>
+        </>
+    );
+};
+
+export const textSection = css`
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+`;
+
+export const notiTopic = css`
+    font-size: ${theme.fontSize.regular};
+    margin: 5px 8px;
+`;
+
+export const notiText = css`
+    color: ${theme.color.grey};
+    font-size: ${theme.fontSize.small};
+    padding: 10px 0px;
+    text-align: left;
+`;
