50cbcf1e5442b18914bed834bfdb5055a38f7a2f
Sang-minKIM <87116017+Sang-minKIM@users.noreply.github.com>
2023-05-30 02:01:39 +0900

    feat: Add go home in 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,10 +1,15 @@
+import { theme } from "@/styles/theme";
 import { css } from "@emotion/react";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Custom404() {
     return (
         <div css={container}>
             <Image css={img} src="/notFound.svg" alt="404" fill />
+            <Link href="/">
+                <div css={home}>홈으로 돌아가기</div>
+            </Link>
         </div>
     );
 }
@@ -20,4 +25,13 @@ const img = css`
     height: 100vh;
     object-fit: cover;
     z-index: -1;
+`;
+
+const home = css`
+    width: 100vw;
+    color: ${theme.color.white};
+    margin: 0 auto;
+    text-align: center;
+    position: absolute;
+    top: 20%;
 `;
