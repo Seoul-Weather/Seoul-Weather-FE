1d3d1d1a19b683614760b3cf6e3d89a272e50425
Sang-minKIM <87116017+Sang-minKIM@users.noreply.github.com>
2023-05-31 16:44:37 +0900

    fix: Remove send notification

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,47 +1,47 @@
 import { useRouter } from "next/router";
 import { useEffect } from "react";
-import { saveSubscription } from "@/utils/db";
+// import { saveSubscription } from "@/utils/db";
 
-export async function sendNotification() {
-    try {
-        const response = await fetch("/api/send-notification", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-        });
-        const data = await response.json();
-        console.log(data);
-    } catch (error) {
-        console.error("Error sending notification:", error);
-    }
-}
+// export async function sendNotification() {
+//     try {
+//         const response = await fetch("/api/send-notification", {
+//             method: "POST",
+//             headers: {
+//                 "Content-Type": "application/json",
+//             },
+//         });
+//         const data = await response.json();
+//         console.log(data);
+//     } catch (error) {
+//         console.error("Error sending notification:", error);
+//     }
+// }
 
-export async function subscribeUser() {
-    navigator.serviceWorker.ready.then((registration) => {
-        registration.pushManager.getSubscription().then((subscription) => {
-            if (subscription) {
-                console.log("Already subscribed");
-            } else {
-                registration.pushManager
-                    .subscribe({
-                        userVisibleOnly: true,
-                        applicationServerKey: process.env.NEXT_PUBLIC_VAPID_PUBLIC_KEY,
-                    })
-                    .then((subscription) => {
-                        // save subscription on DB
-                        fetch("/api/subscribe", {
-                            method: "POST",
-                            headers: {
-                                "Content-Type": "application/json",
-                            },
-                            body: JSON.stringify(subscription),
-                        });
-                    });
-            }
-        });
-    });
-}
+// export async function subscribeUser() {
+//     navigator.serviceWorker.ready.then((registration) => {
+//         registration.pushManager.getSubscription().then((subscription) => {
+//             if (subscription) {
+//                 console.log("Already subscribed");
+//             } else {
+//                 registration.pushManager
+//                     .subscribe({
+//                         userVisibleOnly: true,
+//                         applicationServerKey: process.env.NEXT_PUBLIC_VAPID_PUBLIC_KEY,
+//                     })
+//                     .then((subscription) => {
+//                         // save subscription on DB
+//                         fetch("/api/subscribe", {
+//                             method: "POST",
+//                             headers: {
+//                                 "Content-Type": "application/json",
+//                             },
+//                             body: JSON.stringify(subscription),
+//                         });
+//                     });
+//             }
+//         });
+//     });
+// }
 
 export default function RootRouter() {
     const router = useRouter();
