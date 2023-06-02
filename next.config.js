1f5c7e749f188da59b2f2d7a256b6fb6e1521b13
Sang-minKIM <87116017+Sang-minKIM@users.noreply.github.com>
2023-06-02 14:06:06 +0900

    chore: Add bundle-analyzer

--- next.config.js
+++ next.config.js
@@ -12,9 +12,24 @@ const nextConfig = withPWA({
     reactStrictMode: false,
 });
 
-module.exports = {
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+    enabled: process.env.ANALYZE === "true",
+});
+
+module.exports = withBundleAnalyzer({
     ...nextConfig,
     env: {
         NEXT_PUBLIC_VAPID_PUBLIC_KEY: process.env.NEXT_PUBLIC_VAPID_PUBLIC_KEY,
     },
-};
+    compress: true,
+    webpack(config, { webpack }) {
+        const prod = process.env.NODE_ENV === "production";
+        const plugins = [...config.plugins];
+        return {
+            ...config,
+            mode: prod ? "production" : "development",
+            devtool: prod ? "hidden-source-map" : "eval",
+            plugins,
+        };
+    },
+});
